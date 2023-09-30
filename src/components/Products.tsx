f667039bf66839a78a811240f92886b38724bc63
adebabs-tec <ruqoyatomotayo@gmail.com>
2023-09-30 18:28:48 +0100

    new commit

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import { ProductProps } from '../../type'
+import { FaHeart } from 'react-icons/fa'
+import { HiShoppingCart } from 'react-icons/hi'
 
 const Products = ({ productData }: any) => {
   return (
@@ -11,7 +13,6 @@ const Products = ({ productData }: any) => {
           brand,
           category,
           description,
-
           image,
           isNew,
           oldPrice,
@@ -30,7 +31,16 @@ const Products = ({ productData }: any) => {
                   height={300}
                   className="w-full h-full object-cover scale-90 hover:scale-100 transition-transform duration-300"
                 />
+                <div className="w-12 h-24 absolute bottom-10 right-0 border-[1px] border-gray-400 bg-white rounded-md flex flex-col translate-x-20 group-hover:translate-x-0 duration-300 transition-transform overflow-hidden">
+                  <span className="w-full h-full border-b-[1px] border-b-gray-400 flex justify-center items-center text-xl bg-transparent hover:bg-amazon_yellow cursor-pointer duration-300">
+                    <HiShoppingCart />
+                  </span>
+                  <span className="w-full h-full border-b-[1px] border-b-gray-400 flex justify-center items-center text-xl bg-transparent hover:bg-amazon_yellow cursor-pointer duration-300">
+                    <FaHeart />
+                  </span>
+                </div>
               </div>
+
               <hr />
             </div>
           )
