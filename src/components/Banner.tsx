7db96028e892048111929df2842ae88ea49e603b
adebabs-tec <ruqoyatomotayo@gmail.com>
2023-09-30 05:31:36 +0100

    banner added

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import Image from 'next/image'
+import { Carousel } from 'react-responsive-carousel'
+
+import sliderImg_1 from '../images/slider/sliderImg_1.jpg'
+import sliderImg_2 from '../images/slider/sliderImg_2.jpg'
+import sliderImg_3 from '../images/slider/sliderImg_3.jpg'
 
 const Banner = () => {
-  return <div>Banner</div>
+  return (
+    <div className="relative">
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={3000}
+      >
+        <div>
+          <Image src={sliderImg_1} alt="sliderImg_1" />
+        </div>
+        <div>
+          <Image src={sliderImg_2} alt="sliderImg_2" />
+        </div>
+        <div>
+          <Image src={sliderImg_3} alt="sliderImg_3" />
+        </div>
+      </Carousel>
+      <div className="w-full h-20 bg-gradient-to-t from-gray-100 to-transparent absolute bottom-0 z-20"></div>
+    </div>
+  )
 }
 
 export default Banner
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
-import Footer from '@/components/Footer'
-import BottomHeader from '@/components/header/BottomHeader'
-import Header from '../components/header/Header'
+import Banner from '../components/Banner'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import Products from '@/components/Products'
 
 export default function Home() {
   return (
     <main>
-      <div className="max-w-screen-2xl mx-auto">This is the home page</div>
+      <div className="max-w-screen-2xl mx-auto">
+        <Banner />
+        <Products />
+      </div>
     </main>
   )
 }
