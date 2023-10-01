1e0f218b6649b14b8679327ba9a78103c41bc7d1
adebabs-tec <ruqoyatomotayo@gmail.com>
2023-10-02 00:02:34 +0100

    new commit

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -40,8 +40,7 @@ const Products = ({ productData }: any) => {
                   </span>
                 </div>
               </div>
-
-              <hr />
+              <hr />c
             </div>
           )
         },
